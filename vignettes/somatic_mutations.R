452d1af5e904a54c75c85120d79a713381603bdb
Livius Penter <62758571+shaka87@users.noreply.github.com>
2023-02-14 17:19:36 -0500

    update

--- vignettes/somatic_mutations.R
+++ vignettes/somatic_mutations.R
@@ -2,9 +2,9 @@
 
 library(ggplot2)
 
-knee_plot(x = data.table::fread('./vignettes/data/1002_pileup_TP53_1.csv.gz'), cutoff = 100, downsample = 10000)
+knee_plot(x = data.table::fread('./vignettes/data/1002_pileup_TP53_1.csv.gz'), cutoff = 50, downsample = 10000)
 ggsave('./vignettes/1002_TP53_kneeplot.png', width = 2.5, height = 2)
-knee_plot(x = data.table::fread('./vignettes/data/1002_pileup_U2AF1.csv.gz'), cutoff = 50)
+knee_plot(x = data.table::fread('./vignettes/data/1002_pileup_U2AF1.csv.gz'), cutoff = 20)
 ggsave('./vignettes/1002_U2AF1_kneeplot.png', width = 2.5, height = 2)
 
 TP53 = extract_mutation(BC.data.file = './vignettes/data/1002_pileup_TP53_1.csv.gz', REF = 'G', ALT = 'T', FILTER = 100, downsample = 100000)
