d7c40ebd48823176e4d11766133e8f5b3ebaa64a
Livius Penter <62758571+shaka87@users.noreply.github.com>
2023-02-14 17:20:08 -0500

    update

--- vignettes/somatic_mutations.R
+++ vignettes/somatic_mutations.R
@@ -2,7 +2,7 @@
 
 library(ggplot2)
 
-knee_plot(x = data.table::fread('./vignettes/data/1002_pileup_TP53_1.csv.gz'), cutoff = 50, downsample = 10000)
+knee_plot(x = data.table::fread('./vignettes/data/1002_pileup_TP53_1.csv.gz'), cutoff = 20)
 ggsave('./vignettes/1002_TP53_kneeplot.png', width = 2.5, height = 2)
 knee_plot(x = data.table::fread('./vignettes/data/1002_pileup_U2AF1.csv.gz'), cutoff = 20)
 ggsave('./vignettes/1002_U2AF1_kneeplot.png', width = 2.5, height = 2)
